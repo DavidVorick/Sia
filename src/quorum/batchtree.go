f45364257cb313c279d9aaa80a77a78a007eaeba
DavidVorick <david.vorick@gmail.com>
2014-05-18 21:45:15 -0500

    move file.go into batch.go and batch_tree.go

--- src/quorum/batchtree.go
+++ src/quorum/batchtree.go
@@ -1,27 +1,5 @@
 package quorum
 
-import (
-	"os"
-)
-
-// a BID, or Batch ID, is the global logical address of a batch on Sia. A BID
-// has no relationship to where on disk or in the AA tree the batch is stored.
-// To perform lookups from a BID to the disk location of a BID, a batchMap must
-// be used.
-type BID [32]byte // not exactly sure what BID will end up looking like
-
-// A batch is a group of sectors that is error-corrected together, and is the
-// smallest unit that will move around on the network.
-type batch struct {
-	bid           *BID
-	file          *os.File
-	node          *batchNode
-	sectorLengths []int
-}
-
-// A batchMap maps BIDs to their batch object
-type batchMap map[BID]batch
-
 // batchNode is the basic element in the AA tree that enables efficient
 // selection of random sectors for verification
 type batchNode struct {
@@ -118,7 +96,7 @@ func (parent *batchNode) delete(bn *batchNode) {
 			currentNode.weight += replacementNode.weight
 		}
 
-		// update replacementNode weight and pointeres
+		// update replacementNode weight and pointers
 		replacementNode.left = bn.left
 		replacementNode.right = bn.right
 		if replacementNode.left != nil {
