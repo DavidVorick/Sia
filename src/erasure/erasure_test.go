359d447478704aff7f88d7c8a2c1a8c11dcb383d
DavidVorick <david.vorick@gmail.com>
2014-05-30 20:45:37 -0500

    basic testing for erasure package

--- src/erasure/erasure_test.go
+++ src/erasure/erasure_test.go
@@ -1,9 +1,28 @@
 package erasure
 
 import (
+	"siacrypto"
 	"testing"
 )
 
 func TestEncodeAndRecover(t *testing.T) {
-	// tbi
+	originalData := siacrypto.RandomByteSlice(960)
+	decoded, err := EncodeRedundancy(8, 3, originalData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	corrupted := decoded[3:]
+	indicies := make([]byte, len(corrupted))
+	for index := range indicies {
+		indicies[index] = index+3
+	}
+
+	recoveredData, err := Recover(8, 3, corrupted, indicies)
+
+	for i := range recoveredData {
+		if recoveredData[i] != originalData[i] {
+			t.Fatal("recovered data does not match original data")
+		}
+	}
 }
