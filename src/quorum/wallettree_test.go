9a84112dda19ca866a5598ae71fd5613d5e1b558
DavidVorick <david.vorick@gmail.com>
2014-06-04 18:06:41 -0500

    polish up the test suite and make it more efficient

--- src/quorum/wallettree_test.go
+++ src/quorum/wallettree_test.go
@@ -6,11 +6,9 @@ import (
 	"testing"
 )
 
-// countReachableNodes iterates through a batchTree and counts how many nodes
-// are reachable from the input, which is assumed to be the parent.
-//
-// countReachableNodes does not check for cycles and will get caught in an
-// infinite loop.
+// countReachableNodes iterates through the rbw tree and counts how many nodes
+// can be reached from the input node. It does not check for cycles and will
+// get caught in an infinite loop if there are any cycles.
 func countReachableNodes(w *walletNode) (i int) {
 	if w == nil {
 		return
@@ -22,6 +20,11 @@ func countReachableNodes(w *walletNode) (i int) {
 	return i
 }
 
+// printTree iterates through the rbw tree and prints each element. Because the
+// tree is sorted and has weights, and is printed in DFS order, it's pretty
+// trivial to reconstruct the tree without directly being told which node has
+// which children. I do most of my debugging/reconstruction by hand-drawing the
+// output of this function.
 func printTree(w *walletNode) {
 	if w == nil {
 		return
@@ -32,9 +35,9 @@ func printTree(w *walletNode) {
 	printTree(w.children[1])
 }
 
-// findRedViolatoins iterates through a red-black tree starting with w as the
-// root and verifies that in no situations does a red node have another red
-// node as a child.
+// findRedViolatoins iterates through the rbw tree starting with w as the root
+// and verifies that in no situations does a red node have another red node as
+// a child.
 func findRedViolations(w *walletNode) (violations int) {
 	if w == nil {
 		return
@@ -59,9 +62,9 @@ func findRedViolations(w *walletNode) (violations int) {
 	return violations
 }
 
-// findBlackViolations iterates through a red-black tree starting with w as the
-// root and makes sure that the height of black nodes in the tree is the same
-// at all places in the tree.
+// findBlackViolations iterates through rbw tree starting with w as the root
+// and makes sure that the height of black nodes in the tree is the same at all
+// places in the tree.
 func findBlackViolations(w *walletNode) (height, violations int) {
 	if w == nil {
 		height = 1
@@ -87,9 +90,8 @@ func findBlackViolations(w *walletNode) (height, violations int) {
 	return
 }
 
-// findSortViolaitons iterates through a red-black tree starting with w as the
-// root and makes sure that the entire tree is sorted with no out-of-order
-// elements.
+// findSortViolaitons iterates through the rbw tree starting with w as the root
+// and makes sure that the entire tree is sorted with no out-of-order elements.
 func findSortViolations(w *walletNode) (violations int) {
 	if w == nil {
 		return
@@ -98,7 +100,6 @@ func findSortViolations(w *walletNode) (violations int) {
 	if w.children[0] != nil && w.children[0].id >= w.id {
 		violations += 1
 	}
-
 	if w.children[1] != nil && w.children[1].id <= w.id {
 		violations += 1
 	}
@@ -125,14 +126,12 @@ func findWeightViolations(w *walletNode) (violations int) {
 		selfWeight -= w.children[1].weight
 	}
 	if uint64(selfWeight) != uint64(w.id) {
-		//println("VIOLATION")
-		//println(w.id)
-		//println(selfWeight)
 		violations += 1
 	}
 
 	leftViolations := findWeightViolations(w.children[0])
 	rightViolations := findWeightViolations(w.children[1])
+
 	violations += leftViolations
 	violations += rightViolations
 	return
@@ -144,52 +143,57 @@ func findWeightViolations(w *walletNode) (violations int) {
 // make sense. It does not count the number of reachable nodes because it
 // doesn't know how many nodes are supposed to be reachable. checkViolations
 // takes the testing.T object as input so it can call t.Error directly.
-func checkViolations(id int, w *walletNode, t *testing.T) {
+func checkViolations(location int, w *walletNode, expectedReachable int, t *testing.T) {
 	redViolations := findRedViolations(w)
 	if redViolations != 0 {
-		t.Error(id, "- Got red violations:", redViolations)
+		t.Error(location, "- Got red violations:", redViolations)
 	}
 
 	_, blackViolations := findBlackViolations(w)
 	if blackViolations != 0 {
-		t.Error(id, "- Got black violations:", blackViolations)
+		t.Error(location, "- Got black violations:", blackViolations)
 	}
 
 	sortViolations := findSortViolations(w)
 	if sortViolations != 0 {
-		t.Error(id, "- Got sort violations:", sortViolations)
+		t.Error(location, "- Got sort violations:", sortViolations)
 	}
 
 	weightViolations := findWeightViolations(w)
 	if weightViolations != 0 {
-		t.Fatal(id, "- Got weight violations:", weightViolations)
+		t.Error(location, "- Got weight violations:", weightViolations)
 	}
+
+	reachableNodes := countReachableNodes(w)
+	if reachableNodes != expectedReachable {
+		t.Error(location, "- Wrong number of reachable nodes:", reachableNodes)
+	}
+}
+
+// createTestWallet produces a wallet where the id and the weight are both set
+// to the same number, which is given in the input.
+func createTestWalletNode(id int) (w *walletNode) {
+	w = new(walletNode)
+	w.id = WalletID(id)
+	w.weight = id
+	return
 }
 
 // Extensive testing to make sure that there are no errors with the red-black
 // tree. This test is particularly fleshed out because red-black trees are
 // tricky and I made many mistakes in the first implementation.
 func TestWalletTree(t *testing.T) {
-	// create a quorum and add a bunch of children. 32 is enough children to
-	// create 5 layers, which means all variables in insert are being used. After
-	// each insertion all checks are made to verify that there have been no
-	// violations.
+	// create a quorum and add a few children, just enough to hit a tree height
+	// of 4. After each insertion the tree is verified for integrity.
 	q := new(Quorum)
-	n := 32
+	n := 16
 	for i := 0; i < n; i++ {
-		newWallet := new(walletNode)
-		newWallet.id = WalletID(i)
-		newWallet.weight = i
+		newWallet := createTestWalletNode(i)
 		q.insert(newWallet)
-
-		reachableNodes := countReachableNodes(q.walletRoot)
-		if reachableNodes != i+1 {
-			t.Error("Wrong number of reachable nodes:", reachableNodes)
-		}
-		checkViolations(0, q.walletRoot, t)
+		checkViolations(0, q.walletRoot, i+1, t)
 	}
 
-	// make sure that all 32 of the inserted nodes can be reached.
+	// make sure that all inserted nodes are reachable
 	for i := 0; i < n; i++ {
 		w := q.retrieve(WalletID(i))
 		if w == nil {
@@ -197,8 +201,8 @@ func TestWalletTree(t *testing.T) {
 		}
 	}
 
-	// remove all 32 of the inserted nodes. After each removal, the full set of
-	// tests are used to verify that the tree remains valid.
+	// remove each of the inserted nodes. After each removal, the tree is
+	// verified for integrity.
 	for i := 0; i < n; i++ {
 		q.remove(WalletID(i))
 
@@ -206,28 +210,26 @@ func TestWalletTree(t *testing.T) {
 		if w != nil {
 			t.Error("Maganed to retreive a removed wallet:", i)
 		}
-
-		reachableNodes := countReachableNodes(q.walletRoot)
-		if reachableNodes != n-1-i {
-			t.Error("Wrong number of reachable nodes:", reachableNodes)
-		}
-		checkViolations(1, q.walletRoot, t)
+		checkViolations(1, q.walletRoot, n-1-i, t)
 	}
 
 	if testing.Short() {
 		t.Skip()
 	}
 
-	// insert a bunch of random nodes into the tree, each time verifying that the
-	// tree is still valid.
-	weights := make(map[uint64]bool)
-	n = 1000
+	// The following tests randomly insert and delete nodes in mass in hopes of
+	// hitting every possible test case.
+
+	// Insert many nodes into the tree, each with a random weight. After each
+	// iteration, verify the integrity of the tree.
+	n = 1500
+	weights := make(map[uint64]bool) // keeps track of which elements have been added
 	for i := 0; i < n; i++ {
 		found := false
 		var weight int
 		var err error
 		for !found {
-			weight, err = siacrypto.RandomInt(10000)
+			weight, err = siacrypto.RandomInt(100000)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -238,21 +240,14 @@ func TestWalletTree(t *testing.T) {
 			}
 		}
 
-		node := new(walletNode)
-		node.id = WalletID(weight)
-		node.weight = weight
+		node := createTestWalletNode(weight)
 		q.insert(node)
-
-		reachableNodes := countReachableNodes(q.walletRoot)
-		if reachableNodes != i+1 {
-			t.Error("Wrong number of reachable nodes:", reachableNodes)
-		}
-		checkViolations(2, q.walletRoot, t)
+		checkViolations(2, q.walletRoot, i+1, t)
 	}
 
-	// randomly insert or delete elements
+	// randomly choose between inserting and deleting a random item
 	for i := 0; i < n; i++ {
-		insertOrDelete, err := siacrypto.RandomInt(2)
+		insertOrDelete, err := siacrypto.RandomInt(2) // [0, 2)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -263,7 +258,7 @@ func TestWalletTree(t *testing.T) {
 			var weight int
 			var err error
 			for !found {
-				weight, err = siacrypto.RandomInt(10000)
+				weight, err = siacrypto.RandomInt(100000)
 				if err != nil {
 					t.Fatal(err)
 				}
@@ -274,46 +269,44 @@ func TestWalletTree(t *testing.T) {
 				}
 			}
 
-			node := new(walletNode)
-			node.id = WalletID(weight)
-			node.weight = weight
+			node := createTestWalletNode(weight)
 			q.insert(node)
-
-			checkViolations(4, q.walletRoot, t)
+			checkViolations(4, q.walletRoot, len(weights), t)
 		} else {
 			// delete
-			// turn the weights map into a slice
-			var weightSlice []uint64
-			for key, value := range weights {
-				if value {
-					weightSlice = append(weightSlice, key)
-				}
+			// turn weights into a slice so that a value can be selected at random
+			j := 0
+			weightSlice := make([]uint64, len(weights))
+			for key := range weights {
+				weightSlice[j] = key
+				j++
 			}
 
-			i, err = siacrypto.RandomInt(len(weightSlice))
+			j, err = siacrypto.RandomInt(len(weightSlice))
 			if err != nil {
 				t.Fatal(err)
 			}
-			q.remove(WalletID(weightSlice[i]))
-			checkViolations(5, q.walletRoot, t)
-			weights[weightSlice[i]] = false
-		}
-	}
 
-	// turn the weights map into a slice
-	var weightSlice []uint64
-	for key, value := range weights {
-		if value {
-			weightSlice = append(weightSlice, key)
+			q.remove(WalletID(weightSlice[j]))
+			delete(weights, weightSlice[j])
+			checkViolations(5, q.walletRoot, len(weights), t)
 		}
 	}
 
 	// verify that all weights within the slice are reachable
-	for _, value := range weightSlice {
-		w := q.retrieve(WalletID(value))
+	for key := range weights {
+		w := q.retrieve(WalletID(key))
 		if w == nil {
-			t.Error("Retrieval Error:", value)
+			t.Error("Retrieval Error:", key)
 		}
+	}
+
+	// delete all of the remaining items in the map in a random order
+	i := 0
+	weightSlice := make([]uint64, len(weights))
+	for key := range weights {
+		weightSlice[i] = key
+		i++
 	}
 
 	// shuffle the slice
@@ -332,16 +325,10 @@ func TestWalletTree(t *testing.T) {
 	// remove the elements one at a time
 	for i, v := range weightSlice {
 		q.remove(WalletID(v))
-
 		w := q.retrieve(WalletID(v))
 		if w != nil {
 			t.Error("Maganed to retreive a removed wallet:", v)
 		}
-
-		reachableNodes := countReachableNodes(q.walletRoot)
-		if reachableNodes != len(weightSlice)-1-i {
-			t.Error("Wrong number of reachable nodes:", reachableNodes)
-		}
-		checkViolations(3, q.walletRoot, t)
+		checkViolations(3, q.walletRoot, len(weightSlice)-1-i, t)
 	}
 }
