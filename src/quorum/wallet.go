23e7e9ae246cbdb52ce03c3163e0e8dfc5a63849
DavidVorick <david.vorick@gmail.com>
2014-06-05 17:28:14 -0500

    add Bytes() function to encode wallets to disk

--- src/quorum/wallet.go
+++ src/quorum/wallet.go
@@ -1,27 +1,35 @@
 package quorum
 
 import (
+	"encoding/base64"
+	"fmt"
+	"os"
 	"siacrypto"
+	"strings"
+)
+
+const (
+	scriptPrimerSize = 3532
 )
 
 type WalletID uint64
 type walletHandle [8]byte
 
 type sectorHeader struct {
-	m          byte
-	numSectors byte
+	m        byte
+	numAtoms byte
 }
 
 // A 4kb block of data containing everything about a wallet.
 type wallet struct {
-	walletHash     siacrypto.Hash //hash of the 4064 bytes
+	walletHash     siacrypto.TruncatedHash //hash of the 4064 bytes
 	upperBalance   uint64
 	lowerBalance   uint64
 	scriptAtoms    uint16
 	sectorOverview [256]sectorHeader
 	// above section comes to 564 bytes
 
-	scriptPrimer [3532]byte
+	scriptPrimer [scriptPrimerSize]byte
 	// all together, that's 4kb
 }
 
@@ -42,5 +50,40 @@ func (h walletHandle) id() (b WalletID) {
 	a += uint64(h[0])
 
 	b = WalletID(a)
+	return
+}
+
+func (w *wallet) Bytes() (b [4096]byte) {
+	copy(b[:], w.walletHash[:])
+	offset := siacrypto.TruncatedHashSize
+
+	tmp := w.upperBalance
+	for i := 0; i < 8; i++ {
+		b[offset+i] = byte(tmp)
+		tmp = tmp >> 8
+	}
+	offset += 8
+
+	tmp = w.lowerBalance
+	for i := 0; i < 8; i++ {
+		b[offset+i] = byte(tmp)
+		tmp = tmp >> 8
+	}
+	offset += 8
+
+	tmp16 := w.scriptAtoms
+	for i := 0; i < 2; i++ {
+		b[offset+i] = byte(tmp16)
+		tmp = tmp >> 8
+	}
+	offset += 2
+
+	for i, sector := range w.sectorOverview {
+		b[offset+i*2] = sector.m
+		b[offset+i*2+1] = sector.numAtoms
+	}
+	offset += 2 * len(w.sectorOverview)
+
+	copy(b[offset:], w.scriptPrimer[:])
 	return
 }
