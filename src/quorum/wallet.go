91dafb3e714200434186803e77a8a457067e43b0
DavidVorick <david.vorick@gmail.com>
2014-06-05 08:07:23 -0500

    add encodings from WalletID to walletHandle and back

--- src/quorum/wallet.go
+++ src/quorum/wallet.go
@@ -5,21 +5,42 @@ import (
 )
 
 type WalletID uint64
+type walletHandle [8]byte
 
 type sectorHeader struct {
-	m byte
+	m          byte
 	numSectors byte
 }
 
 // A 4kb block of data containing everything about a wallet.
 type wallet struct {
-	walletHash siacrypto.Hash //hash of the 4064 bytes
-	upperBalance uint64
-	lowerBalance uint64
-	scriptAtoms uint32
-	sectorOverview [256]SectorHeader
+	walletHash     siacrypto.Hash //hash of the 4064 bytes
+	upperBalance   uint64
+	lowerBalance   uint64
+	scriptAtoms    uint32
+	sectorOverview [256]sectorHeader
 	// above section comes to 564 bytes
 
 	scriptPrimer [3532]byte
 	// all together, that's 4kb
+}
+
+func (id WalletID) Handle() (b walletHandle) {
+	for i := 0; i < 8; i++ {
+		b[i] = byte(id)
+		id = id >> 8
+	}
+	return
+}
+
+func (h walletHandle) ID() (b WalletID) {
+	var a uint64
+	for i := 7; i > 0; i-- {
+		a += uint64(h[i])
+		a = a << 8
+	}
+	a += uint64(h[0])
+
+	b = WalletID(a)
+	return
 }
