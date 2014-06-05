4bf10ce0973c055227f19d8eb7e625d2dcfb4d21
DavidVorick <david.vorick@gmail.com>
2014-06-05 07:23:01 -0500

    Create wallet struct
    A one atom struct that houses all of the important data to a wallet.
    Wallets are kept on disk instead of in memory, because with scripts they
    can get very large. Also a participant could theoretically be required
    to store 16 million (more realistically just a few thousand).

--- src/quorum/wallet.go
+++ src/quorum/wallet.go
@@ -1,3 +1,25 @@
 package quorum
 
+import (
+	"siacrypto"
+)
+
 type WalletID uint64
+
+type sectorHeader struct {
+	m byte
+	numSectors byte
+}
+
+// A 4kb block of data containing everything about a wallet.
+type wallet struct {
+	walletHash siacrypto.Hash //hash of the 4064 bytes
+	upperBalance uint64
+	lowerBalance uint64
+	scriptAtoms uint32
+	sectorOverview [256]SectorHeader
+	// above section comes to 564 bytes
+
+	scriptPrimer [3532]byte
+	// all together, that's 4kb
+}
