d0aaae086f8e81f323cbcaea704a1ce5bcca9056
DavidVorick <david.vorick@gmail.com>
2014-06-08 18:07:02 -0500

    add TestBlockHistoryHeaderEncoding

--- src/participant/block_test.go
+++ src/participant/block_test.go
@@ -1,8 +1,41 @@
 package participant
 
 import (
+	"siacrypto"
 	"testing"
 )
+
+func TestBlockHistoryHeaderEncoding(t *testing.T) {
+	var bhh blockHistoryHeader
+	bhh.latestBlock = uint32(siacrypto.RandomUInt64())
+	for i := range bhh.blockOffsets {
+		bhh.blockOffsets[i] = uint32(siacrypto.RandomUInt64())
+	}
+
+	bhhBytes, err := bhh.GobEncode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(bhhBytes) != BlockHistoryHeaderSize {
+		t.Error("Length of bhhBytes is not equal to BlockHistoryHeaderSize")
+	}
+
+	var newBhh blockHistoryHeader
+	err = newBhh.GobDecode(bhhBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if newBhh.latestBlock != bhh.latestBlock {
+		t.Error("Encoded and Decoded bhh.latestBlock do not match")
+	}
+	for i := range bhh.blockOffsets {
+		if newBhh.blockOffsets[i] != bhh.blockOffsets[i] {
+			t.Error("Encoded and decoded bhh.blockoffsets do not match for index", i)
+		}
+	}
+}
 
 func TestBlockEncoding(t *testing.T) {
 }
