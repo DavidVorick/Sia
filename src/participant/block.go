3d18ca488f492ccdad3c70730be26a57ca643a5b
DavidVorick <david.vorick@gmail.com>
2014-06-08 20:29:27 -0500

    document block.go

--- src/participant/block.go
+++ src/participant/block.go
@@ -9,17 +9,21 @@ import (
 )
 
 const (
-	SnapshotLen            = 100
+	SnapshotLen            = 100 // number of blocks separating each snapshot
 	BlockHistoryHeaderSize = 4 + SnapshotLen*4 + siacrypto.TruncatedHashSize*SnapshotLen
 )
 
+// A block is just a collection of heartbeats, along with information about
+// which block it is and which block was it's parent.
 type block struct {
-	hash       siacrypto.TruncatedHash
 	height     uint32
 	parent     siacrypto.TruncatedHash
 	heartbeats [quorum.QuorumSize]*heartbeat
 }
 
+// the blockHistoryHeader is the header that preceeds the block history file,
+// containing an index of all the blocks in the history and their data offsets
+// in the file.
 type blockHistoryHeader struct {
 	latestBlock  uint32
 	blockOffsets [SnapshotLen]uint32
@@ -74,8 +78,6 @@ func (b *block) Hash() (hash siacrypto.TruncatedHash, err error) {
 	return
 }
 
-// The GobEncode for a block does not include the hash, that must be encoded
-// separately.
 func (b *block) GobEncode() (gobBlock []byte, err error) {
 	// get the height as a [4]byte
 	intb := siaencoding.UInt32ToByte(b.height)
@@ -129,7 +131,6 @@ func (b *block) GobEncode() (gobBlock []byte, err error) {
 	return
 }
 
-// GobDecode for a block does not look for a hash
 func (b *block) GobDecode(gobBlock []byte) (err error) {
 	if b == nil {
 		err = fmt.Errorf("cannot decode into nil value")
@@ -209,12 +210,25 @@ func (b *block) GobDecode(gobBlock []byte) (err error) {
 	return
 }
 
+// SaveBlock takes a block, which is just a list of heartbeats attatched to
+// non-tossed participants, and saves it to disk. Blocks are saved in chains of
+// SnapshotLen, after which a new chain is created and the oldest is deleted.
+// Two chains total are kept around, one complete chain and one yet-incomplete
+// chain. These chains are purely so other hosts have large windows to download
+// the quorum and synchronize with their siblings.
 func (p *Participant) SaveBlock(b *block) (err error) {
+	// if p.activHistoryStep == SnapshotLen, it's time to cycle the histories by
+	// deleting the oldest one and create a new one. Otherwise you just append to
+	// the existing and yet-incomplete history.
 	var file *os.File
 	if p.activeHistoryStep == SnapshotLen {
+		// reset history step, delete old history, migrate recently-completed
+		// history.
 		p.activeHistoryStep = 0
 		os.Remove(p.recentHistory)
 		p.recentHistory = p.activeHistory
+
+		// find a name for the new history and create a file for it
 		p.activeHistory = p.quorum.GetWalletPrefix()
 		p.activeHistory += fmt.Sprintf(".blockHistory.%v", b.height)
 		file, err = os.Create(p.activeHistory)
@@ -223,6 +237,7 @@ func (p *Participant) SaveBlock(b *block) (err error) {
 		}
 		defer file.Close()
 	} else {
+		// increase the active step and open the existing file for writing.
 		p.activeHistoryStep += 1
 		file, err = os.OpenFile(p.activeHistory, os.O_RDWR, 0666)
 		if err != nil {
@@ -231,6 +246,8 @@ func (p *Participant) SaveBlock(b *block) (err error) {
 		defer file.Close()
 	}
 
+	// if p.activeHistoryStep == 0, there is nothing to load. Otherwise, the saved
+	// blockHistoryHeader must be loaded from the file into memory.
 	var bhh blockHistoryHeader
 	if p.activeHistoryStep != 0 {
 		blockHistoryHeaderBytes := make([]byte, BlockHistoryHeaderSize)
@@ -244,43 +261,45 @@ func (p *Participant) SaveBlock(b *block) (err error) {
 		}
 	}
 
+	// encode the block so it can be saved to disk
 	gobBlock, err := b.GobEncode()
 	if err != nil {
 		return
 	}
 
+	// if the latest block is 0, then the struct is empty and the first index
+	// needs to account for the bhh struct. If the latest block is SnapshotLen-1,
+	// then it is the last block and updating the following index will cause a
+	// panic
 	if bhh.latestBlock == 0 {
 		bhh.blockOffsets[0] = uint32(BlockHistoryHeaderSize)
-		bhh.blockOffsets[1] = uint32(BlockHistoryHeaderSize)
 	}
-
 	if bhh.latestBlock != SnapshotLen-1 {
-		bhh.blockOffsets[bhh.latestBlock+1] += uint32(len(gobBlock))
+		bhh.blockOffsets[bhh.latestBlock+1] += uint32(len(gobBlock)) + bhh.blockOffsets[bhh.latestBlock]
 	}
 
-	_, err = file.Seek(int64(bhh.blockOffsets[bhh.latestBlock]), 0)
-	if err != nil {
-		panic(err)
-	}
-
-	n, err := file.Write(gobBlock)
-	if err != nil || n != len(gobBlock) {
-
-	}
-
+	// seek back to 0 to write the updated bhh struct to disk
 	_, err = file.Seek(0, 0)
 	if err != nil {
 		panic(err)
 	}
-
 	encodeBHH, err := bhh.GobEncode()
 	if err != nil {
 		panic(err)
 	}
-
-	n, err = file.Write(encodeBHH)
+	n, err := file.Write(encodeBHH)
 	if err != nil || n != len(encodeBHH) {
 		panic(err)
+	}
+
+	// seek to the offset location in the file to write the block to disk
+	_, err = file.Seek(int64(bhh.blockOffsets[bhh.latestBlock]), 0)
+	if err != nil {
+		panic(err)
+	}
+	n, err = file.Write(gobBlock)
+	if err != nil || n != len(gobBlock) {
+
 	}
 
 	return
