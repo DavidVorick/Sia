a1de07fe3f127ec09ed9c71465630a5a24860d43
lukechampine <luke.champine@gmail.com>
2014-06-12 00:29:11 -0400

    add encoding for floats

---
+++ src/siaencoding/floats.go
@@ -0,0 +1,26 @@
+package siaencoding
+
+import (
+	"encoding/binary"
+	"math"
+)
+
+func Float32ToByte(f float32) (b [4]byte) {
+	binary.LittleEndian.PutUint32(b[:], math.Float32bits(f))
+	return
+}
+
+func Float32FromByte(b [4]byte) (f float32) {
+	f = math.Float32frombits(binary.LittleEndian.Uint32(b[:]))
+	return
+}
+
+func Float64ToByte(f float64) (b [4]byte) {
+	binary.LittleEndian.PutUint64(b[:], math.Float64bits(f))
+	return
+}
+
+func Float64FromByte(b [4]byte) (f float64) {
+	f = math.Float64frombits(binary.LittleEndian.Uint64(b[:]))
+	return
+}
