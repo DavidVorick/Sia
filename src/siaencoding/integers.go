aac93230d47be3f96ce4efbcc2e1acaffa5de42e
lukechampine <luke.champine@gmail.com>
2014-06-12 01:59:13 -0400

    add encoding for ints

--- src/siaencoding/integers.go
+++ src/siaencoding/integers.go
@@ -23,3 +23,23 @@ func DecUint64(b []byte) (i uint64) {
 	i = binary.LittleEndian.Uint64(b)
 	return
 }
+
+func EncInt32(i int32) (b [4]byte) {
+	binary.LittleEndian.PutUint32(b[:], uint32(i))
+	return
+}
+
+func DecInt32(b []byte) (i int32) {
+	i = int32(binary.LittleEndian.Uint32(b))
+	return
+}
+
+func EncInt64(i int64) (b [8]byte) {
+	binary.LittleEndian.PutUint64(b[:], uint64(i))
+	return
+}
+
+func DecInt64(b []byte) (i int64) {
+	i = int64(binary.LittleEndian.Uint64(b))
+	return
+}
