ff9794f9277527e74f7e7acf95b5f9e22686f861
lukechampine <luke.champine@gmail.com>
2014-06-13 02:44:11 -0400

    squash last few compile-time errors

--- src/quorum/script/interpreter_test.go
+++ src/quorum/script/interpreter_test.go
@@ -1,27 +1,33 @@
 package script
 
 import (
-	"bytes"
+	"quorum"
 	"testing"
 )
 
 func TestOpCodes(t *testing.T) {
-	s := new(Script)
+	// create script execution environment
+	q := new(quorum.Quorum)
+	q.CreateBootstrapWallet(1, quorum.NewBalance(0, 15000), []byte{0x2D})
+	si := &ScriptInput{
+		WalletID: 1,
+	}
+
 	// test jump
-	s.Block = []byte{
+	si.Input = []byte{
 		0x01, 0x02, // push 2
 		0x01, 0x03, // push 3
 		// if 2 == 3, jump ahead 10 instructions (causing an error)
 		0x16, 0x1F, 0x00, 0x0A,
 		0xFF,
 	}
-	_, err := s.Execute(nil, nil)
+	_, err := si.Execute(q)
 	if err != nil {
 		t.Fatal("wrong execution path taken:", err)
 	}
 
 	// test store/load
-	s.Block = []byte{
+	si.Input = []byte{
 		0x01, 0x02, // push 2
 		0x04,       // dup 2
 		0x21, 0x01, // store 2 in register 1
@@ -30,49 +36,38 @@ func TestOpCodes(t *testing.T) {
 		0x17, 0x1F, 0x00, 0x0A,
 		0xFF,
 	}
-	_, err = s.Execute(nil, nil)
+	_, err = si.Execute(q)
 	if err != nil {
 		t.Fatal("wrong execution path taken:", err)
 	}
 
 	// test invalid scripts
-	s.Block = []byte{
+	si.Input = []byte{
 		0x01, 0xAA,
 		0x06, 0xFF,
 	}
-	_, err = s.Execute(nil, nil)
+	_, err = si.Execute(q)
 	if err == nil {
 		t.Fatal("expected stack empty error")
 	}
-	s.Block = []byte{
+	si.Input = []byte{
 		0x11,
 	}
-	_, err = s.Execute(nil, nil)
+	_, err = si.Execute(q)
 	if err == nil {
 		t.Fatal("expected missing argument error")
 	}
-	s.Block = nil
-	_, err = s.Execute(nil, nil)
+	si.Input = nil
+	_, err = si.Execute(q)
 	if err == nil {
 		t.Fatal("expected missing terminator error")
 	}
-	s.Block = []byte{
+	si.Input = []byte{
 		0x25, 0xFF, 0xF6,
 		0xFF,
 	}
-	_, err = s.Execute(nil, nil)
+	_, err = si.Execute(q)
 	if err == nil {
 		t.Fatal("expected out of bounds error")
-	}
-}
-
-func BenchmarkInterpreter(b *testing.B) {
-	s := new(Script)
-	// push a thousand bytes and perform various operations on them
-	s.Block = bytes.Repeat([]byte{0x01, 0xAA}, 1000)
-	s.Block = append(s.Block, bytes.Repeat([]byte{0x05, 0x06, 0x07, 0x08, 0x09, 0x0A}, 100)...)
-	s.Block = append(s.Block, 0xFF)
-	for n := 0; n < b.N; n++ {
-		s.Execute(nil, nil)
 	}
 }
