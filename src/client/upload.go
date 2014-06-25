f9786acdea684d8ebde63502d90ecc83288e29e8
DavidVorick <david.vorick@gmail.com>
2014-06-25 15:45:31 -0500

    add RSEncode to quorum

--- src/client/upload.go
+++ src/client/upload.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"fmt"
+	"io"
 	"math"
 	"network"
 	"os"
@@ -9,8 +10,8 @@ import (
 	"quorum"
 )
 
-func CalculateAtoms(filename string, m byte) (atoms int, err error) {
-	multiplier := float64(m) / float64(quorum.QuorumSize)
+func CalculateAtoms(filename string, k byte) (atoms int, err error) {
+	multiplier := float64(k) / float64(quorum.QuorumSize)
 	file, err := os.Open(filename)
 	if err != nil {
 		return
@@ -27,7 +28,7 @@ func CalculateAtoms(filename string, m byte) (atoms int, err error) {
 	return
 }
 
-func (c *Client) UploadFile(id quorum.WalletID, filename string, m byte) {
+func (c *Client) UploadFile(id quorum.WalletID, filename string, k byte) {
 	var siblings [quorum.QuorumSize]*quorum.Sibling
 	err := c.router.SendMessage(&network.Message{
 		Dest: participant.BootstrapAddress,
@@ -37,8 +38,43 @@ func (c *Client) UploadFile(id quorum.WalletID, filename string, m byte) {
 	})
 	if err != nil {
 		fmt.Printf("Upload: Error: %v\n", err)
+		return
 	}
 
 	// take the file and produce a bunch of erasure coded atoms written one piece
 	// at a time to be MerkleCollapsed and then uploaded to the siblings.
+	// the approach is to create a bunch of files, one for each erasure coded section
+	var writerSegments [quorum.QuorumSize]io.Writer
+	var fileSegments [quorum.QuorumSize]*os.File
+	for i := range fileSegments {
+		tmpname := fmt.Sprintf("%s/%s.%v.tmp", os.TempDir(), filename, i)
+		file, err := os.Create(tmpname)
+		if err != nil {
+			fmt.Printf("Upload: Error: %v\n", err)
+			return
+		}
+		defer func(file *os.File, tmpname string) {
+			file.Close()
+			os.Remove(tmpname)
+		}(file, tmpname)
+		writerSegments[i] = file
+		fileSegments[i] = file
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Upload: Error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	err = quorum.RSEncode(file, writerSegments, k)
+	if err != nil {
+		fmt.Printf("Upload: Error: %v\n", err)
+		return
+	}
+
+	// Now that the files have been written to 1 atom at a time, rewind them to
+	// the beginning and create diffs for each file. Then upload the diffs to
+	// each silbing via RPC
 }
