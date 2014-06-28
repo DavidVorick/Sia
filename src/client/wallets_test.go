0295d0186406a93168f318150e45fb2dd58f793c
JoshVorick <josh.vorick@gmail.com>
2014-06-27 21:41:40 -0500

    Update wllaet test

--- src/client/wallets_test.go
+++ src/client/wallets_test.go
@@ -3,31 +3,40 @@ package client
 import (
 	"math/rand"
 	"os"
+	"quorum"
+	"siacrypto"
 	"testing"
 )
 
 func TestSaveLoadWallet(t *testing.T) {
-	var wal0, wal1 *Wallet
-	wal0 = new(Wallet)
-	wal1 = new(Wallet)
-	wal0.ID = rand.Uint32()
-	wal0.Type = "Generic"
-	err := SaveWallet(wal0.ID, wal0.Type, "tempFile")
+	var (
+		id1, id2           quorum.WalletID
+		keypair1, keypair2 *siacrypto.Keypair
+		err                error
+	)
+	keypair1 = new(siacrypto.Keypair)
+	keypair2 = new(siacrypto.Keypair)
+	id1 = quorum.WalletID(uint64(rand.Uint32()) * uint64(rand.Uint32())) //There isn't a Uint64()
+	keypair1.PK, keypair1.SK, err = siacrypto.CreateKeyPair()
 	if err != nil {
 		t.Fatal(err)
 	}
-	wal1, err = LoadWallet("tempFile")
+	err = SaveWallet(id1, keypair1, "tempFile")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
-	if wal0.ID != wal1.ID {
+	id2, keypair2, err = LoadWallet("tempFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if id1 != id2 {
 		t.Fatal("Wallet ID not preserved when loaded")
 	}
-	if wal0.Type != wal1.Type {
-		t.Fatal("Wallet Type not preserved when loaded")
+	if keypair1.PK.Compare(keypair2.PK) == false {
+		t.Fatal("Wallet's key pair not preserved when loaded")
 	}
-	err = os.Remove("tempFile")
-	if err != nil {
-		t.Fatal(err)
+	if keypair1.SK.Compare(keypair2.SK) == false {
+		t.Fatal("Wallet's key pair not preserved when loaded")
 	}
+	_ = os.Remove("tempFile")
 }
