9cf4511f7393fd7cf17494973f5004409e34f099
JoshVorick <josh.vorick@gmail.com>
2014-06-29 17:17:53 -0500

    Remove superfluous panics

--- src/client/wallets.go
+++ src/client/wallets.go
@@ -9,10 +9,6 @@ import (
 )
 
 func SaveWallet(id quorum.WalletID, keypair *siacrypto.Keypair, destFile string) (err error) {
-	if destFile == "" {
-		fmt.Errorf("Cannot save, file name is empty string")
-		return
-	}
 	if keypair == nil {
 		fmt.Errorf("Cannot encode nil key pair")
 		return
@@ -25,56 +21,53 @@ func SaveWallet(id quorum.WalletID, keypair *siacrypto.Keypair, destFile string)
 	idSlice := siaencoding.EncUint64(uint64(id))
 	pubSlice, err := keypair.PK.GobEncode()
 	if err != nil {
-		panic(err)
+		return
 	}
 	secSlice, err := keypair.SK.GobEncode()
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	f, err := os.Create(destFile)
 	if err != nil {
-		panic(err)
+		return
 	}
 	defer f.Close()
 
 	_, err = f.Write(idSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	_, err = f.Write(pubSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	_, err = f.Write(secSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	return
 }
 
 func LoadWallet(fileName string) (id quorum.WalletID, keypair *siacrypto.Keypair, err error) {
-	if fileName == "" {
-		err = fmt.Errorf("Cannot load, file name is empty string")
-	}
 	f, err := os.Open(fileName)
 	if err != nil {
-		panic(err)
+		return
 	}
 	idSlice := make([]byte, quorum.WalletIDSize)
 	pubSlice := make([]byte, siacrypto.PublicKeySize)
 	secSlice := make([]byte, siacrypto.SecretKeySize)
 	_, err = f.Read(idSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	_, err = f.Read(pubSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	_, err = f.Read(secSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	id = quorum.WalletID(siaencoding.DecUint64(idSlice))
 	keypair = new(siacrypto.Keypair)
@@ -82,11 +75,11 @@ func LoadWallet(fileName string) (id quorum.WalletID, keypair *siacrypto.Keypair
 	keypair.SK = new(siacrypto.SecretKey)
 	err = keypair.PK.GobDecode(pubSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	err = keypair.SK.GobDecode(secSlice)
 	if err != nil {
-		panic(err)
+		return
 	}
 	return
 }
