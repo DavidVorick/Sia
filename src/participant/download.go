feab32ec56a5a53728baddcf59997f015ab1800a
DavidVorick <david.vorick@gmail.com>
2014-06-30 00:49:24 -0500

    switch particpant download to taking requests for specific atoms

--- src/participant/download.go
+++ src/participant/download.go
@@ -1,12 +1,26 @@
 package participant
 
 import (
+	"fmt"
 	"os"
 	"quorum"
 )
 
-func (p *Participant) DownloadSegment(id quorum.WalletID, segment *[]byte) (err error) {
-	filename := p.quorum.SectorFilename(id)
+const (
+	MaxAtomsPerDownload = 16
+)
+
+type AtomDownload struct {
+	ID           quorum.WalletID
+	AtomIndicies []uint16
+}
+
+func (p *Participant) DownloadSegment(ad AtomDownload, segment *[]byte) (err error) {
+	if len(ad.AtomIndicies) > MaxAtomsPerDownload {
+		ad.AtomIndicies = ad.AtomIndicies[:MaxAtomsPerDownload]
+	}
+
+	filename := p.quorum.SectorFilename(ad.ID)
 	file, err := os.Open(filename)
 	if err != nil {
 		return
@@ -14,11 +28,24 @@ func (p *Participant) DownloadSegment(id quorum.WalletID, segment *[]byte) (err 
 	defer file.Close()
 
 	info, err := file.Stat()
-	if err != nil {
-		return
-	}
 
-	*segment = make([]byte, info.Size())
-	_, err = file.Read(*segment)
+	*segment = make([]byte, len(ad.AtomIndicies)*quorum.AtomSize)
+	for i := range ad.AtomIndicies {
+		seekTo := int64(quorum.AtomSize) * int64(1+ad.AtomIndicies[i])
+		if seekTo+int64(quorum.AtomSize) < info.Size() {
+			err = fmt.Errorf("Invalid index request: sector is not composed of that many atoms!")
+			return
+		}
+
+		_, err = file.Seek(int64(quorum.AtomSize)*int64(1+ad.AtomIndicies[i]), 0)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err := file.Read((*segment)[quorum.AtomSize*i : quorum.AtomSize*(i+1)])
+		if err != nil {
+			panic(err)
+		}
+	}
 	return
 }
