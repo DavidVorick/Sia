705085df73e921c5bf6c6b6a447749c3b3c243f5
lukechampine <luke.champine@gmail.com>
2014-08-05 12:59:12 -0400

    RPC call = remote procedure call call

--- src/consensus/api.go
+++ src/consensus/api.go
@@ -5,7 +5,7 @@ import (
 	"state"
 )
 
-// Block is an RPC call that returns a block of a specific height. Participants
+// Block is an RPC that returns a block of a specific height. Participants
 // only keep a history of so many blocks, so asking for future blocks or
 // expired blocks will return an error.
 func (p *Participant) Block(blockHeight uint32, block *delta.Block) (err error) {
@@ -13,14 +13,13 @@ func (p *Participant) Block(blockHeight uint32, block *delta.Block) (err error) 
 	return
 }
 
-// Metadata is an RPC call that returns the current state metadata, found in
-// p.engine.state.Metadata
+// Metadata is an RPC that returns the current state metadata.
 func (p *Participant) Metadata(_ struct{}, smd *state.StateMetadata) (err error) {
 	*smd = p.engine.Metadata()
 	return
 }
 
-// UpdateSegment is an RPC call that allows hosts to submit diffs that match
+// UpdateSegment is an RPC that allows hosts to submit diffs that match
 // updates that have been confirmed by consensus.
 func (p *Participant) UpdateSegment(sd delta.SegmentDiff, accepted *bool) (err error) {
 	*accepted, err = p.engine.UpdateSegment(sd)
