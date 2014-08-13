95344df3f6771f0c251284f2d37cf81493e3bdb7
lukechampine <luke.champine@gmail.com>
2014-08-13 12:53:35 -0400

    update script opcodes

--- delta/scripts.go
+++ delta/scripts.go
@@ -54,9 +54,9 @@ func AddSiblingInput(wid state.WalletID /*, deadline uint64*/, sib *state.Siblin
 	}
 	si.Input = appendAll(
 		[]byte{
-			0x25, 0x08, 0x00, // move data pointer to encoded sibling
-			0xE3, //             push encoded sibling
-			0x31, //             call AddSibling
+			0x33, 0x08, 0x00, // move data pointer to encoded sibling
+			0xE4, //             push encoded sibling
+			0x41, //             call AddSibling
 			0xFF, //             exit
 		},
 		encSib,
@@ -105,8 +105,8 @@ func ResizeSectorEraseInput(atoms uint16, m byte) []byte {
 func ProposeUploadInput(encUA []byte) []byte {
 	return append([]byte{
 		0x33, 0x08, 0x00, // move data pointer to encoded args
-		0xE2, 0x01, //       read args into buffer 1
-		0x45, 0x01, //       call ProposeUpload
+		0xE4, //             push args
+		0x45, //             call ProposeUpload
 		0xFF, //             exit
 	}, encUA...)
 }
@@ -114,24 +114,24 @@ func ProposeUploadInput(encUA []byte) []byte {
 // The BootstrapScript acts as a fountain, and can be called to spawn a new
 // wallet with a minimal starting balance that will the economy to get flowing.
 var BootstrapScript = []byte{
-	0x27, 0x08, //       00 push 8 bytes of input (wallet id)
-	0x01, 0x00, //       02 push 1 (high balance)
-	0x02, 0xA8, 0x61, // 04 push 100 (low balance)
-	0x2E, 0x01, //       08 read rest of input into buffer 1
-	0x32, 0x01, //       10 call create wallet
-	0xFF, //             12 exit
+	0x34, 0x08, //       00 push 8 bytes of input (wallet id)
+	0x01, 0x00, //       02 push 0 (high balance)
+	0x02, 0xA8, 0x61, // 04 push 25000 (low balance)
+	0xE4, //             07 push script
+	0x32, //             08 call CreateWallet
+	0xFF, //             10 exit
 }
 
 // DefaultScript returns a script that verifies a signature, and transfers
 // control to the input if the verification was successful.
 func DefaultScript(publicKey siacrypto.PublicKey) []byte {
 	return append([]byte{
-		0x26, 0x10, 0x00, // 00 move data pointer to public key
-		0x39, 0x20, 0x01, // 03 read public key into buffer 1
-		0x2E, 0x02, //       06 read signed message into buffer 2
-		0x34, 0x01, 0x02, // 08 verify signature
-		0x38,             // 11 if invalid signature, reject
-		0x26, 0x70, 0x00, // 12 move data pointer to input body
-		0x2F, //             15 execute input
+		0x32, 0x0B, 0x00, // 00 move data pointer to public key
+		0x34, 0x20, //       03 push public key
+		0xE4,             // 05 push signed input
+		0x40,             // 06 verify signature
+		0xE5,             // 07 if invalid signature, reject
+		0x32, 0x4C, 0x00, // 08 move data pointer to input body (12 + PublicKeySize + SignatureSize)
+		0x38, //             11 execute input
 	}, publicKey[:]...)
 }
