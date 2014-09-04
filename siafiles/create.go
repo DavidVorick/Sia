f64c4ffd535d22a6f1d3cc6ab6df3acb09d2f579
David Vorick <david.vorick@gmail.com>
2014-09-04 16:07:11 -0400

    add home directory support to siafiles

--- siafiles/create.go
+++ siafiles/create.go
@@ -2,28 +2,80 @@ package siafiles
 
 import (
 	"encoding/base64"
+	"errors"
 	"os"
+	"os/user"
 	"path"
 )
 
-// TempDir is the folder where Sia-specific temporary files, such as
-// files created during testing, are stored.
-var TempDir string
+var siahome string
+var homefound bool // using homefound is more secure than checking homdir == ""
 
+// Homedir returns the users home directory.
+func HomeDir() (homedir string, err error) {
+	if !homefound {
+		err = errors.New("no known home directory")
+		return
+	}
+	homedir = siahome
+
+	return
+}
+
+// TempDir returns the Sia temp directory.
+func TempDir() string {
+	return path.Join(os.TempDir(), "Sia")
+}
+
+// siafiles.init scans the directory structure of Sia, and creates necessary
+// folders.
 func init() {
-	// initialize the temp directory
-	TempDir = os.TempDir() + "/Sia"
-	if err := os.MkdirAll(TempDir, os.ModeDir|os.ModePerm); err != nil {
+	// Create a temp directory for Sia.
+	if err := os.MkdirAll(TempDir(), os.ModeDir|os.ModePerm); err != nil {
+		// Perhaps something more gentle can be done?
 		panic(err)
+	}
+
+	// Scan for a home folder.
+	homefound = false
+	usr, err := user.Current()
+	if err != nil {
+		// Perhaps a more gentle action can be performed?
+		panic(err)
+	}
+	stdHome := path.Join(usr.HomeDir, ".config", "Sia")
+	if Exists(stdHome) {
+		siahome = stdHome
+		homefound = true
+	} else {
+		badHome := path.Join(usr.HomeDir, ".Sia")
+		if Exists(badHome) {
+			siahome = badHome
+			homefound = true
+		}
 	}
 }
 
-// TempFilename returns a safe concatenation of the input filename
-// and the temp directory.
-func TempFilename(name string) string {
-	return path.Join(TempDir, name)
+// HomeFilename returns a safe concetenation of the input filename and the home
+// directory.
+func HomeFilename(name string) (fullpath string, err error) {
+	homedir, err := HomeDir()
+	if err != nil {
+		return
+	}
+	fullpath = path.Join(homedir, name)
+	return
 }
 
+// SafeFilename takes a byteslice and converts it to a string that can be used
+// as a filename without casuing errors/unexpected behavior within the
+// filesystem.
 func SafeFilename(b []byte) (s string) {
 	return base64.URLEncoding.EncodeToString(b)
+}
+
+// TempFilename returns a safe concatenation of the input filename and the temp
+// directory.
+func TempFilename(name string) string {
+	return path.Join(TempDir(), name)
 }
