7351f83df319311f2e96760f295960f2a36ba680
David Vorick <david.vorick@gmail.com>
2014-09-09 18:28:40 -0400

    add modes, wallets, participants, and settings

--- client-termbox/main.go
+++ client-termbox/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 
 	"github.com/nsf/termbox-go"
@@ -27,7 +26,12 @@ type Context struct {
 	SettingsActive     bool
 }
 
-var context Context
+var (
+	width  int
+	height int
+
+	context Context
+)
 
 // Draw uses the context field to determine what functions to call when drawing
 // the image. Siabox uses a box-style of programming, each function receives a
@@ -35,57 +39,8 @@ var context Context
 // where that box is.
 func draw() {
 	// Get size of whole window.
-	w, h := termbox.Size()
-
-	// Determine how to draw the home field.
-	var homeSeparator int
-	if w <= HomeBoxWidth {
-		// If there isn't enough room for the home box, then just draw
-		// the home box as red. This will be context dependent, the
-		// program will try to have enough room for whatever box the
-		// context says is active.
-	} else {
-		homeSeparator = HomeBoxWidth
-	}
-
-	// Draw the home box.
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	for y := 0; y < h; y++ {
-		termbox.SetCell(homeSeparator, y, ' ', termbox.ColorDefault, DividerColor)
-	}
-
-	// Write the home fields.
-	for x, c := range "Sia Alpha v3" {
-		termbox.SetCell(x+Border, Border, c, HomeHeaderColor, termbox.ColorDefault)
-	}
-	for x, c := range "Wallets" {
-		if context.WalletsActive {
-			termbox.SetCell(x+Border, 2*Border+1, c, HomeActiveColor, termbox.ColorDefault)
-		} else {
-			termbox.SetCell(x+Border, 2*Border+1, c, HomeInactiveColor, termbox.ColorDefault)
-		}
-	}
-	for x, c := range "Participants" {
-		if context.ParticipantsActive {
-			termbox.SetCell(x+Border, 4*Border+1, c, HomeActiveColor, termbox.ColorDefault)
-		} else {
-			termbox.SetCell(x+Border, 4*Border+1, c, HomeInactiveColor, termbox.ColorDefault)
-		}
-	}
-	for x, c := range "Settings" {
-		if context.SettingsActive {
-			termbox.SetCell(x+Border, 6*Border+1, c, HomeActiveColor, termbox.ColorDefault)
-		} else {
-			termbox.SetCell(x+Border, 6*Border+1, c, HomeInactiveColor, termbox.ColorDefault)
-		}
-	}
-
-	// Fill remaining space with random colors.
-	for y := 0; y < h; y++ {
-		for x := homeSeparator + 1; x < w; x++ {
-			termbox.SetCell(x, y, ' ', termbox.ColorDefault, termbox.Attribute(rand.Int()%8)+1)
-		}
-	}
+	width, height = termbox.Size()
+	drawHome()
 	termbox.Flush()
 }
 
---
+++ client-termbox/participants.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math/rand"
+
+	"github.com/nsf/termbox-go"
+)
+
+// Draw the wallets section in the priary screen.
+func drawParticipants(startColumn int) {
+	// Fill remaining space with random colors.
+	for y := 0; y < height; y++ {
+		for x := startColumn; x < width; x++ {
+			termbox.SetCell(x, y, ' ', termbox.ColorDefault, termbox.Attribute(rand.Int()%2)+3)
+		}
+	}
+}
