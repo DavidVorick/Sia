0fd39405e4ca88b5ab9a609bbbc5e1e56398a341
David Vorick <david.vorick@gmail.com>
2014-09-10 17:59:15 -0400

    add flags for connecting to the server

--- client-termbox/main.go
+++ client-termbox/main.go
@@ -1,5 +1,15 @@
 package main
 
+type Config struct {
+	Port uint16
+
+	ServerHostname string
+	ServerPort     uint16
+	ServerID       uint8
+}
+
+var config Config
+
 func main() {
-	termboxRun()
+	manageCommands()
 }
