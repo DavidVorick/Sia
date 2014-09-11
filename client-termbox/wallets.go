ac0058e38e79db3737dafafe15ea173f0149ec8f
David Vorick <david.vorick@gmail.com>
2014-09-11 16:36:28 -0400

    network call to fetch ids

--- client-termbox/wallets.go
+++ client-termbox/wallets.go
@@ -3,7 +3,8 @@ package main
 import (
 	"math/rand"
 
-	//"github.com/NebulousLabs/Sia/network"
+	"github.com/NebulousLabs/Sia/network"
+	"github.com/NebulousLabs/Sia/state"
 
 	"github.com/nsf/termbox-go"
 )
@@ -11,6 +12,17 @@ import (
 // Draw the wallets section in the priary screen.
 func drawWallets(startColumn int) {
 	// Fetch a list of wallets from the server.
+	var ids *[]state.WalletID
+	err := networkState.Router.SendMessage(network.Message{
+		Dest: networkState.ServerAddress,
+		Proc: "Server.WalletIDs",
+		Args: struct{}{},
+		Resp: &ids,
+	})
+	if err != nil {
+		// Draw an error message.
+		return
+	}
 
 	// Fill remaining space with random colors.
 	for y := 0; y < context.Height; y++ {
