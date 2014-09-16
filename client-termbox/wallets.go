0fcf01e58280b8247c8437d2f29378820b68a614
David Vorick <david.vorick@gmail.com>
2014-09-16 11:16:42 -0400

    comment out wallet-request code

--- client-termbox/wallets.go
+++ client-termbox/wallets.go
@@ -3,26 +3,28 @@ package main
 import (
 	"math/rand"
 
-	"github.com/NebulousLabs/Sia/network"
-	"github.com/NebulousLabs/Sia/state"
+	//"github.com/NebulousLabs/Sia/network"
+	//"github.com/NebulousLabs/Sia/state"
 
 	"github.com/nsf/termbox-go"
 )
 
 // Draw the wallets section in the priary screen.
 func drawWallets(startColumn int) {
-	// Fetch a list of wallets from the server.
-	var ids *[]state.WalletID
-	err := networkState.Router.SendMessage(network.Message{
-		Dest: networkState.ServerAddress,
-		Proc: "Server.WalletIDs",
-		Args: struct{}{},
-		Resp: &ids,
-	})
-	if err != nil {
-		// Draw an error message.
-		return
-	}
+	/*
+		// Fetch a list of wallets from the server.
+		var ids *[]state.WalletID
+		err := networkState.Router.SendMessage(network.Message{
+			Dest: networkState.ServerAddress,
+			Proc: "Server.WalletIDs",
+			Args: struct{}{},
+			Resp: &ids,
+		})
+		if err != nil {
+			// Draw an error message.
+			// return
+		}
+	*/
 
 	// Fill remaining space with random colors.
 	for y := 0; y < context.Height; y++ {
