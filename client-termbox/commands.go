14df2167b015e45ea7bdd65d60deda09c7b01938
lukechampine <luke.champine@gmail.com>
2014-09-16 11:54:37 -0400

    condense main routine

--- client-termbox/commands.go
+++ client-termbox/commands.go
@@ -8,28 +8,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type Config struct {
+	Port   uint16
+	Server network.Address
+	Router *network.RPCServer
+}
+
+// global config variable
+var config Config
+
 func commandStart(cmd *cobra.Command, args []string) {
 	var err error
-	networkState.Router, err = network.NewRPCServer(config.Port)
+	config.Router, err = network.NewRPCServer(config.Port)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	networkState.ServerAddress = network.Address{
-		Host: config.ServerHostname,
-		Port: config.ServerPort,
-		ID:   network.Identifier(config.ServerID),
-	}
-
 	termboxRun()
 }
 
-func version(cmd *cobra.Command, args []string) {
-	fmt.Println("Sia Termbox Client v0.0.2.1")
-}
-
-func manageCommands() {
+func main() {
 	root := &cobra.Command{
 		Use:   "client-termbox",
 		Short: "Sia Client v0.0.2.1",
@@ -37,18 +36,19 @@ func manageCommands() {
 		Run:   commandStart,
 	}
 
-	version := &cobra.Command{
+	root.AddCommand(&cobra.Command{
 		Use:   "version",
 		Short: "Print version information",
 		Long:  "Prints version information about the Sia Termbox Client",
-		Run:   version,
-	}
+		Run:   func(_ *cobra.Command, _ []string) { fmt.Println("Sia Termbox Client v0.0.2.1") },
+	})
 
 	root.Flags().Uint16VarP(&config.Port, "port", "p", 9980, "Which port the client messenger should listen on")
-	root.Flags().StringVarP(&config.ServerHostname, "server-hostname", "H", "localhost", "The hostname of the server you are connecting to.")
-	root.Flags().Uint16VarP(&config.ServerPort, "server-port", "P", 9988, "The port on which the server is listening.")
-	root.Flags().Uint8VarP(&config.ServerID, "server-id", "I", 1, "The id of the server you are connecting to.")
+	root.Flags().StringVarP(&config.Server.Host, "server-hostname", "H", "localhost", "The hostname of the server you are connecting to.")
+	root.Flags().Uint16VarP(&config.Server.Port, "server-port", "P", 9988, "The port on which the server is listening.")
+	var id uint8
+	root.Flags().Uint8VarP(&id, "server-id", "I", 1, "The id of the server you are connecting to.")
+	config.Server.ID = network.Identifier(id)
 
-	root.AddCommand(version)
 	root.Execute()
 }
