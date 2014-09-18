17ca13e29476614f4eff87c1bc03c11158e70a74
lukechampine <luke.champine@gmail.com>
2014-09-18 13:45:50 -0400

    remove Section abstraction

--- client-termbox/settings.go
+++ client-termbox/settings.go
@@ -5,60 +5,57 @@ import (
 )
 
 const (
-	SectionColor = termbox.ColorWhite
-	FieldColor   = termbox.ColorBlue
+	FieldColor = termbox.ColorBlue
 )
 
 type Field struct {
-	name  string
-	width int
-}
-
-type Section struct {
 	DefaultView
-	title  string
-	fields []Field
+	name   string
+	width  int
+	offset int
 }
 
-func (s *Section) Draw() {
-	clearRectangle(s.Rectangle)
-	drawString(s.MinX+1, s.MinY+1, s.title, SectionColor, termbox.ColorDefault)
-	for i, f := range s.fields {
-		drawString(s.MinX+5, s.MinY+i+2, f.name, SectionColor, termbox.ColorDefault)
-		drawLine(s.MinX+6+len(f.name), s.MinY+i+2, f.width+1, FieldColor)
-	}
+func (f *Field) Focus() {
+	f.hasFocus = true
+	// move cursor to first field
+	termbox.SetCursor(f.MinX+1+len(f.name), f.MinY)
 }
 
-func (s *Section) HandleInput(key termbox.Key) {
-	switch key {
-	case termbox.KeyArrowLeft:
-		s.GiveFocus(s.Parent)
-	}
+func (f *Field) Draw() {
+	clearRectangle(f.Rectangle)
+	drawString(f.MinX, f.MinY, f.name, termbox.ColorWhite, termbox.ColorDefault)
+	drawLine(f.MinX+len(f.name)+1, f.MinY, f.width, termbox.ColorBlue)
+}
+
+func (f *Field) HandleKey(key termbox.Key) {
 }
 
 type SettingsView struct {
 	DefaultView
-	sections []Section
-	sel      int
+	fields []*Field
+	sel    int
 }
 
 func (sv *SettingsView) SetDims(r Rectangle) {
 	sv.Rectangle = r
-	for i := range sv.sections {
-		sv.sections[i].SetDims(r)
-		r.MinY += len(sv.sections[i].fields)*2 + 1
+	for _, f := range sv.fields {
+		f.SetDims(Rectangle{
+			MinX: r.MinX + 1,
+			MinY: r.MinY + f.offset,
+			MaxX: r.MinX + len(f.name) + f.width + 2,
+			MaxY: r.MinY + f.offset + 1,
+		})
 	}
 }
 
 func (sv *SettingsView) Focus() {
-	sv.hasFocus = true
-	// move cursor to first field
-	termbox.SetCursor(sv.MinX+6+len(sv.sections[0].fields[0].name), sv.MinY+2)
+	// focus the first field
+	sv.GiveFocus(sv.fields[0])
 }
 
 func (sv *SettingsView) Draw() {
-	for i := range sv.sections {
-		sv.sections[i].Draw()
+	for i := range sv.fields {
+		sv.fields[i].Draw()
 	}
 }
 
@@ -67,25 +64,31 @@ func (sv *SettingsView) HandleKey(key termbox.Key) {
 	case termbox.KeyArrowLeft:
 		termbox.HideCursor()
 		sv.GiveFocus(sv.Parent)
+	case termbox.KeyArrowUp:
+		if sv.sel > 0 {
+			sv.sel--
+		}
+		sv.GiveFocus(sv.fields[sv.sel])
+	case termbox.KeyArrowDown:
+		if sv.sel+1 < len(sv.fields) {
+			sv.sel++
+		}
+		sv.GiveFocus(sv.fields[sv.sel])
 	}
 }
 
 func newSettingsView(parent View) View {
 	sv := &SettingsView{
-		sections: []Section{
-			{title: "Client", fields: []Field{
-				{name: "Port:", width: 20},
-			}},
-			{title: "Server", fields: []Field{
-				{name: "Host:", width: 20},
-				{name: "Port:", width: 20},
-				{name: "ID:  ", width: 20},
-			}},
+		fields: []*Field{
+			{name: "Client Port:", width: 20, offset: 1},
+			{name: "Server Host:", width: 20, offset: 3},
+			{name: "Server Port:", width: 20, offset: 4},
+			{name: "Server ID:  ", width: 20, offset: 5},
 		},
 	}
 	sv.Parent = parent
-	for i := range sv.sections {
-		sv.sections[i].Parent = sv
+	for i := range sv.fields {
+		sv.fields[i].Parent = sv
 	}
 	return sv
 }
